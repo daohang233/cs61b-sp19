11039cfc2ba90c4c8b9c9a56b57f8697e592bce8
Daohang <daohsong@gmail.com>
2019-03-24 21:56:59 +0800

    complete lab1

--- lab1/HelloNumbers.java
+++ lab1/HelloNumbers.java
@@ -2,7 +2,7 @@ public class HelloNumbers {
     public static void main(String args[]) {
         int x = 0;
         int sum = 0;
-        while(x < 10){
+        while(x < 10) {
             sum += x;
             System.out.print(sum + " ");
             x++;
